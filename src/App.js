0d5b176ec248c7ea7e78a6aa6e79206e74bac4dc
Kyle Tilley <katilley10@gmail.com>
2020-08-12 16:12:25 -0600

    need to fix what is going on with VScode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>hi</div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+
+
+  render () {
+    return (
+      <div className="container">
+        <div className="header-container">header</div>
+      </div>
+    );
+  }
 }
 
 export default App;
