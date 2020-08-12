e76e8bab880ebc0f8d74c737575b1ec23836dfb5
Kyle Tilley <katilley10@gmail.com>
2020-08-12 17:12:08 -0600

    finally placed logo where I want it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import logo from './logo.png';
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -14,7 +16,26 @@ class App extends React.Component {
   render () {
     return (
       <div className="container">
-        <div className="header-container">header</div>
+        <div className="header-container">
+          <div className="logo">
+            <a href="https://www.ir.com/" >
+              <img className="logo-img" src={logo} alt="IR logo" />
+            </a>
+          </div>
+          {/* <div className="">Prognosis</div> */}
+          <div className="header-left">
+            <a className="left" href="google.com">Home</a>
+          </div>
+          <div className="header-left">
+            <a className="left" href="google.com">Reports</a>
+          </div>
+          <div className="header-left">
+            <a className="left" href="google.com">Network</a>
+          </div>
+          <div className="header-left">
+            <a className="left" href="google.com">Alerts</a>
+          </div>
+        </div>
       </div>
     );
   }
