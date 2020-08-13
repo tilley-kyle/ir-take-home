ac1c4341f97942f8284f2090ec6d7ce0f489bc17
Kyle Tilley <katilley10@gmail.com>
2020-08-12 21:09:53 -0600

    done for the night. In the middle of working on fun for the logo for 'user' in the top right corner

--- src/App.js
+++ src/App.js
@@ -18,23 +18,30 @@ class App extends React.Component {
       <div className="container">
         <div className="header-container">
           <div className="logo">
-            <a href="https://www.ir.com/" >
+            <a href="https://www.ir.com/" target="_blank" rel="noopener noreferrer">
               <img className="logo-img" src={logo} alt="IR logo" />
             </a>
           </div>
           {/* <div className="">Prognosis</div> */}
           <div className="header-left first">
-            <a className="left" href="google.com">Home</a>
+            <a className="left" href="google.com" target="_blank" rel="noopener noreferrer">Home</a>
           </div>
           <div className="header-left">
-            <a className="left" href="google.com">Reports</a>
+            <a className="left" href="google.com" target="_blank" rel="noopener noreferrer">Reports</a>
           </div>
           <div className="header-left">
-            <a className="left" href="google.com">Network</a>
+            <a className="left" href="google.com" target="_blank" rel="noopener noreferrer">Network</a>
           </div>
           <div className="header-left">
-            <a className="left" href="google.com">Alerts</a>
+            <a className="left" href="https://www.youtube.com/watch?v=On3vPlqcsLo" target="_blank" rel="noopener noreferrer">Alerts</a>
           </div>
+          <div className="header-left right">
+            <a className="symbol" href="google.com" target="_blank" rel="noopener noreferrer" ></a>
+          </div>
+          <div className="header-left right">
+            <a className="left" href="http://www.kyleatilley.com/" target="_blank" rel="noopener noreferrer">Contact Us</a>
+          </div>
+
         </div>
       </div>
     );
