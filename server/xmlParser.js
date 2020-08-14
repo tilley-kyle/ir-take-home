10c9be562a657183b683fdc5a5500556c06d0c7f
Kyle Tilley <katilley10@gmail.com>
2020-08-14 16:35:03 -0600

    capturing the tag properly

--- server/xmlParser.js
+++ server/xmlParser.js
@@ -1,16 +1,22 @@
 const xmlParser = (req, res, xml) => {
   const parsedData = {};
   const tagsToSearchFor = ['SystemUnit', 'Peripherals', 'Cameras', 'Call', 'Network', 'Capabilities', 'SystemTime', 'ContactInfo', 'Status'];
+  const startIndex = xml.indexOf('?>') + 1;
 
-
-
-  let startIndex = xml.indexOf('?>') + 1;
-  const recursorFunction = (xmlString, obj) => {
-
+  const recursorFunction = (xmlString, obj, currentParentTag) => {
+    for (let i = 0; i < xmlString.length; i++) {
+      //if CE is < create new key in obj through next >
+      if (xmlString[i] === '<') {
+        const endOfTag = xmlString.slice(i).indexOf('>');
+        const tag = xmlString.slice(i + 1, endOfTag + i);
+      }
+      //if CE is > AND next element IS NOT < set following values till next < to the value of key
+      //if CE is > AND next element IS <, call recursor function
+      //if CE is / AND next element IS > return out of recursor
+    }
   }
 
-  recursorFunction(xml, parsedData);
-  console.log(parsedData);
+  recursorFunction(xml.slice(startIndex), parsedData, '');
 }
 
 module.exports = xmlParser;
