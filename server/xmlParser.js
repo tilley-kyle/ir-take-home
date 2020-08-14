36cf0ff1cd7c72c6a1f315e75f70deb85a05016e
Kyle Tilley <katilley10@gmail.com>
2020-08-14 17:27:34 -0600

    there is a bug that is jumping my 'i' value ahead but I'm so close to being on track!

--- server/xmlParser.js
+++ server/xmlParser.js
@@ -6,21 +6,22 @@ const xmlParser = (req, res, xml) => {
   const recursorFunction = (xmlString, obj, currentParentTag) => {
     let tag = '';
     for (let i = 0; i < xmlString.length; i++) {
+      console.log(i, ':', xmlString.slice(i))
       //if CE is < create new key in obj through next >
       if (xmlString[i] === '<' && xmlString[i + 1] !== '/') {
         const endOfTag = xmlString.slice(i).indexOf('>');
         tag = xmlString.slice(i + 1, endOfTag + i);
-        i += endOfTag; //takes the function out of the closing > of the tag
-        console.log(xmlString[i])
+        i += endOfTag - 1; //takes the function out of the closing > of the tag
+        console.log(i)
       }
 
       //if CE is > AND next element IS NOT < set following values till next < to the value of key
-      else if (xmlString[i - 1] === '<') {
-        // console.log(i, ':', xmlString[i])
+      else if (xmlString[i] === '<') {
         const endOfProperty = xmlString.slice(i).indexOf('<');
         const propertyValue = xmlString.slice(i + 1, endOfProperty + 1);
+        console.log(i, ':', endOfProperty)
+        console.log(xmlString.slice(i))
         obj[tag] = propertyValue;
-        console.log('prop: ', endOfProperty)
       }
       //if CE is > AND next element IS <, call recursor function
       //if CE is / AND next element IS > return out of recursor
