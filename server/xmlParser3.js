6aa324bc5622fa094e18f67379eb092ab538df88
Kyle Tilley <katilley10@gmail.com>
2020-08-15 22:44:06 -0600

    who knows really

--- server/xmlParser3.js
+++ server/xmlParser3.js
@@ -6,21 +6,18 @@ const xmlParser = (req, res, xml) => {
   const recursor = (xmlString, obj) => {
 
     for (let i = 0; i < xmlString.length; i++) {
-      //if we're at the base level (value is right after key) set tag = value
-      //else if there is a nested tag, set  obj[tag] = recursor
-      //just be baller
-      if (xmlString[i] === '<' && xmlString[i + 1] !== '/') {
-        const endOfTag = xmlString.indexOf('>');
-        const tag = xmlString.slice(i + 1, endOfTag);
-        console.log('tag: ', tag);
-        i += endOfTag + 1;
-        if (xmlString.slice(i).indexOf('\n') === 0) {
-          i += xmlString.slice(i).indexOf('<');
-          obj[tag] = { tag: recursor(xmlString.slice(i), obj) };
-        } else if (xmlString.slice(i).indexOf('\n') !== 0) {
-          obj[tag] =   xmlString.slice(i, xmlString.slice(i).indexOf('<') + i);;
-        }
+      const tag = xmlString.slice(1, xmlString.indexOf('>'));
+      console.log(xmlString.slice(tag.length + 2).indexOf('\n') === 0)
+      if (xmlString.slice(tag.length + 2).indexOf('\n') === 0) { //if there is nested layer
+        let innerString = xmlString.slice(tag.length + 2);
+        innerString = innerString.slice(innerString.indexOf('<'));
+        const innerTag = innerString.slice(1, innerString.indexOf('>'));
+        console.log('new: ', innerTag)
+        obj[tag] = 'not ready'
       }
+      // if (xmlString.slice(i).indexOf('\n') === 0) {
+      //   console.log('there')
+      // }
     }
   }
 
